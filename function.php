b4c8c363770750d33a3117aaa84a13e74f6be962
antara001 <antarans481@gmail.com>
2026-04-27 14:57:14 +0600

    First commit

---
+++ function.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     function cube($num){
+     return $num * $num * $num;
+     }
+     echo cube(4);
+    ?>
+</body>
+</html>
